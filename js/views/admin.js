7d5d14c3e41134e71eaf7d83e68425b7f5ac8cb0
aliekberayric-ai <aliekberayric@gmail.com>
2026-04-01 16:22:00 +0200

    Update admin.js

--- js/views/admin.js
+++ js/views/admin.js
@@ -54,7 +54,7 @@ import {
   listInfoPopupsAdmin,
   // createInfoPopup, //
   updateInfoPopup,
-  deleteInfoPopup 
+ // deleteInfoPopup 
 } from "../modules/infoPopups.js"; 
 
 /* -----------------------------------------------------------
