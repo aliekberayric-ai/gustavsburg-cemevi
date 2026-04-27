9dd5f8a104210088e9a95c65c6a69fd9abc7f36b
aliekberayric-ai <aliekberayric@gmail.com>
2026-04-27 19:41:48 +0200

    Update admin.js

--- js/views/admin.js
+++ js/views/admin.js
@@ -50,12 +50,12 @@ import {
   deleteHomeTile
 } from "../modules/homeTiles.js";
 
-/*import {
+import {
   listInfoPopupsAdmin,
   createInfoPopup,
   updateInfoPopup,
   deleteInfoPopup
-} from "../modules/infoPopups.js"; */
+} from "../modules/infoPopups.js"; 
 
 /* -----------------------------------------------------------
    HELPERS
