bbde5b4985aee3a5afab5f5c9b4ed02b52b17ac3
aliekberayric-ai <aliekberayric@gmail.com>
2026-05-02 23:28:03 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
  * - Central place for config
  */
 
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const SUPABASE_URL = "https://qhxswxevocfvmtwutghk.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_CQhc7cbuR0ypSfEJ7MHGmw_4v7rCy5f";
